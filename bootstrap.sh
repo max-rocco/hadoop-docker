af73fb4094451a90c99918116eef0bc9bcd6298f
Max Rocco <76984797+max-rocco@users.noreply.github.com>
2021-07-30 11:19:01 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,12 @@
 
 # 2. Connect to "localhost" via SSH
 # https://askubuntu.com/a/123080
-ssh -oStrictHostKeyChecking=no localhost
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t localhost << EOF
 
-# 3. Source the environment variables dumped in /etc/enviroment
-source /etc/environment
+  # 3. Source the environment variables dumped in /etc/enviroment
+  source /etc/environment
 
-# 4. Set Hadoop's "sbin" & "bin" in path
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+  # 4. Set Hadoop's "sbin" & "bin" in path
+  export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+
+EOF
